f6059113214efe8ae0ce0ae087c0f43975249789
Seiyon Arulampalam <sarulampalam3@gatech.edu>
2024-06-09 08:50:15 -0400

    Modified the original code to time it the same way the jit and gpu code are times.

--- 1D-Thermal/cupy/cpu.py
+++ 1D-Thermal/cupy/cpu.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from pyinstrument import Profiler
+import time
 
 np.set_printoptions(precision=4)
 # plt.style.use("dark_background")
@@ -280,7 +281,7 @@ def Integrate_f(
 apply_convection = False  # apply forced convection at tip of beam
 
 # Establish the total number of elements and nodes and beam length
-num_elems = 5
+num_elems = 20_000
 num_nodes = num_elems + 1
 L = 0.05  # length of beam [m]
 D = 0.02  # diameter of rod [m]
@@ -303,134 +304,154 @@ c1 = rho * cp * A
 a = k * A
 c0 = P * beta
 
-# Generate the mesh
-xloc = np.linspace(0, L, num_nodes)  # location of x nodes
+start = time.perf_counter()  # start timer
+for i in range(6):
+    print(f"Iteration : {i}:")
+    start_mesh = time.perf_counter()  # start timer
+    # Generate the mesh
+    xloc = np.linspace(0, L, num_nodes)  # location of x nodes
 
-# Create an array that maps node tag to node position: | node tag | node location
-node_array = np.zeros((num_nodes, 2))
-for i in range(num_nodes):
-    node_array[i][0] = i
-    node_array[i][1] = xloc[i]
+    # Create an array that maps node tag to node position: | node tag | node location
+    node_array = np.zeros((num_nodes, 2))
+    for i in range(num_nodes):
+        node_array[i][0] = i
+        node_array[i][1] = xloc[i]
 
-print("Mapping node tag -> node location:")
-print(node_array)
-print()
+    # print("Mapping node tag -> node location:")
+    # print(node_array)
+    # print()
 
-# Create element array: | element # | node 1 position | node 2 position |
-element_array = np.zeros((num_elems, 3))
-for i in range(num_elems):
-    element_array[i][0] = i
-    element_array[i][1] = node_array[i][1]
-    element_array[i][2] = node_array[i + 1][1]
+    # Create element array: | element # | node 1 position | node 2 position |
+    element_array = np.zeros((num_elems, 3))
+    for i in range(num_elems):
+        element_array[i][0] = i
+        element_array[i][1] = node_array[i][1]
+        element_array[i][2] = node_array[i + 1][1]
 
-print("Mapping element tag -> node position:")
-print(element_array)
-print()
+    # print("Mapping element tag -> node position:")
+    # print(element_array)
+    # print()
 
-# Create element node tag array: | element # | node 1 tag | node 2 tag |
-element_node_tag_array = np.zeros((num_elems, 3))
-for i in range(num_elems):
-    element_node_tag_array[i][0] = int(i)
-    element_node_tag_array[i][1] = int(node_array[i][0])
-    element_node_tag_array[i][2] = int(node_array[i + 1][0])
+    # Create element node tag array: | element # | node 1 tag | node 2 tag |
+    element_node_tag_array = np.zeros((num_elems, 3))
+    for i in range(num_elems):
+        element_node_tag_array[i][0] = int(i)
+        element_node_tag_array[i][1] = int(node_array[i][0])
+        element_node_tag_array[i][2] = int(node_array[i + 1][0])
 
-print("Mapping element tag -> node tag:")
-print(element_node_tag_array)
-print()
+    # print("Mapping element tag -> node tag:")
+    # print(element_node_tag_array)
+    # print()
+    end_mesh = time.perf_counter()  # end timer
+    total_time_mesh = end_mesh - start_mesh
+    print(f"    Time to generate mesh : {total_time_mesh:.6e} s")
 
+    # Assemble K matrix, F vector, C Matrix
+    # FEA: K*u + C*u_dot = F
+    start_sys = time.perf_counter()  # start timer
 
-# Assemble K matrix, F vector, C Matrix
-# FEA: K*u + C*u_dot = F
+    # Excitation of the beam
+    g = np.zeros(num_elems)  # initialize excitation vector
 
-# Excitation of the beam
-g = np.zeros(num_elems)  # initialize excitation vector
+    # Initialize the vectors and matrixes for the finite element analysis
+    K_global = np.zeros((num_nodes, num_nodes))
+    F_global = np.zeros(num_nodes)
+    C_global = np.zeros((num_nodes, num_nodes))
 
-# Initialize the vectors and matrixes for the finite element analysis
-K_global = np.zeros((num_nodes, num_nodes))
-F_global = np.zeros(num_nodes)
-C_global = np.zeros((num_nodes, num_nodes))
+    # define key function handles for gaussian integration
+    wts, xi_pts = GaussOrder4()
+    jac_func = Order1Jacobian
+    shape_func_derivatives = Order1_dNdx
+    shape_func_vals = Order1ShapeFunc
 
-# define key function handles for gaussian integration
-wts, xi_pts = GaussOrder4()
-jac_func = Order1Jacobian
-shape_func_derivatives = Order1_dNdx
-shape_func_vals = Order1ShapeFunc
+    for e in range(num_elems):
+        # loop through each element and update the global matrix
+        x_left = element_array[e][1]
+        x_right = element_array[e][2]
+        x_vec = np.array([x_left, x_right])
 
-profiler = Profiler()
-profiler.start()
-for e in range(num_elems):
-    # loop through each element and update the global matrix
-    x_left = element_array[e][1]
-    x_right = element_array[e][2]
-    x_vec = np.array([x_left, x_right])
+        K_local = np.zeros((2, 2))
+        for i in range(2):
+            for j in range(2):
+                K_local[i, j] = Integrate_K(
+                    wts,
+                    xi_pts,
+                    jac_func,
+                    shape_func_derivatives,
+                    shape_func_vals,
+                    x_vec,
+                    a,
+                    c0,
+                    i,
+                    j,
+                )
+                n_i_e = element_node_tag_array[e][i + 1]
+                n_j_e = element_node_tag_array[e][j + 1]
+                K_global[int(n_i_e), int(n_j_e)] += K_local[i, j]
 
-    K_local = np.zeros((2, 2))
-    for i in range(2):
-        for j in range(2):
-            K_local[i, j] = Integrate_K(
+        f_local = np.zeros(2)
+        for i in range(2):
+            f_local[i] = Integrate_f(
                 wts,
                 xi_pts,
                 jac_func,
-                shape_func_derivatives,
                 shape_func_vals,
                 x_vec,
-                a,
-                c0,
+                g[e],
                 i,
-                j,
             )
             n_i_e = element_node_tag_array[e][i + 1]
-            n_j_e = element_node_tag_array[e][j + 1]
-            K_global[int(n_i_e), int(n_j_e)] += K_local[i, j]
+            F_global[int(n_i_e)] += f_local[i]
 
-    f_local = np.zeros(2)
-    for i in range(2):
-        f_local[i] = Integrate_f(
-            wts,
-            xi_pts,
-            jac_func,
-            shape_func_vals,
-            x_vec,
-            g[e],
-            i,
-        )
-        n_i_e = element_node_tag_array[e][i + 1]
-        F_global[int(n_i_e)] += f_local[i]
+    end_sys = time.perf_counter()  # end timer
+    total_time_sys = end_sys - start_sys
+    print(f"    Time to assemble K and F : {total_time_sys:.6e} s")
 
-profiler.stop()
-print(profiler.output_text(unicode=True, color=True))
+    start_bc = time.perf_counter()  # start timer
+    # Apply Dirichlet B.C.
+    # modify K_global to be a pivot
+    K_global[0, 0] = 1.0
+    K_global[0, 1:] = 0.0  # zero the 1st row of the K matrix
 
+    # modify the F_global due to dirichlet BC
+    F_global[0] = u_root
+    F_global[1:] = F_global[1:] - K_global[1:, 0] * u_root
 
-# Apply Dirichlet B.C.
-# modify K_global to be a pivot
-K_global[0, 0] = 1.0
-K_global[0, 1:] = 0.0  # zero the 1st row of the K matrix
+    # Zero the 1st col of the K matrix
+    K_global[1:, 0] = 0.0
 
-# modify the F_global due to dirichlet BC
-F_global[0] = u_root
-F_global[1:] = F_global[1:] - K_global[1:, 0] * u_root
+    # # Apply convection BC at beam tip
+    if apply_convection == True:
+        K_global[-1, -1] += beta * A
 
-# Zero the 1st col of the K matrix
-K_global[1:, 0] = 0.0
+    end_bc = time.perf_counter()  # end timer
+    total_time_bc = end_bc - start_bc
+    print(f"    Time to apply BC : {total_time_bc:.6e} s")
 
-# # Apply convection BC at beam tip
-if apply_convection == True:
-    K_global[-1, -1] += beta * A
+    # print("K matrix Modified:")
+    # print(K_global)
+    # print()
+    # print("F vector Modified:")
+    # print(F_global)
+    # print()
 
-print("K matrix Modified:")
-print(K_global)
-print()
-print("F vector Modified:")
-print(F_global)
-print()
+    # Solve system fo Equations
+    start_solve = time.perf_counter()  # start timer
+    steady_state_soln = np.linalg.solve(K_global, F_global)
+    end_solve = time.perf_counter()  # end timer
+    total_time_solve = end_solve - start_solve
+    print(f"    Time to solve Kx = F : {total_time_solve:.6e} s\n")
 
-# Solve system fo Equations
-steady_state_soln = np.linalg.solve(K_global, F_global)
+end = time.perf_counter()  # end timer
+total_time = end - start
+print("\n-------------------------------------")
+print(f"Total Time  : {total_time:.6e} s")
+print(f"Total Elems : {num_elems}")
+print("-------------------------------------")
 
-print("Solution Steady State:")
-print(steady_state_soln)
-print()
-
+# print("Solution Steady State:")
+# print(steady_state_soln)
+# print()
 
 # Plot Results
 fig, ax = plt.subplots(nrows=1, ncols=1)
