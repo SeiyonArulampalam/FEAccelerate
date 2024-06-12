73474783689f98271371cf877b0cfca0b542d82b
Seiyon Arulampalam <sarulampalam3@gatech.edu>
2024-06-12 15:09:13 -0400

    Updated to return the connectivity array for the mesh.

--- 2D-Maxwell/geometry.py
+++ 2D-Maxwell/geometry.py
@@ -1,9 +1,9 @@
 import gmsh
 import sys
-
-gmsh.initialize()
-gmsh.model.add("model")
-
+import numpy as np
+import time
+from tabulate import tabulate
+from numba import njit
 
 """
  --------------------------------------------------------
@@ -19,7 +19,16 @@ gmsh.model.add("model")
 """
 
 
-def create_mesh(B=200, H=100, l1=20, l2=30, d1=50, d2=30, lc=5):
+def create_mesh(
+    B=200,
+    H=100,
+    l1=20,
+    l2=30,
+    d1=50,
+    d2=30,
+    lc=1,
+    flag=False,
+):
     """
     _summary_
 
@@ -40,7 +49,8 @@ def create_mesh(B=200, H=100, l1=20, l2=30, d1=50, d2=30, lc=5):
     lc : float, optional
          mesh refinement parameter, by default 1
     """
-    gmsh.initialize()
+    gmsh.initialize(sys.argv)
+    gmsh.option.setNumber("General.Terminal", 0)  # 0 = no print, 1 = print
     gmsh.model.add("model")
     prob = gmsh.model.geo
 
@@ -101,11 +111,119 @@ def create_mesh(B=200, H=100, l1=20, l2=30, d1=50, d2=30, lc=5):
     gmsh.model.mesh.generate(2)
 
     # Launch the GUI to see the results:
-    if "-nopopup" not in sys.argv:
-        gmsh.fltk.run()
+    if flag == True:
+        if "-nopopup" not in sys.argv:
+            gmsh.fltk.run()
+
+    nodeTags, nodeCoords, _ = gmsh.model.mesh.getNodes(
+        dim=-1,
+        tag=-1,
+    )
+
+    get_element_types = gmsh.model.mesh.getElementTypes()
+    linear_tri_elem_type = get_element_types[1]  # 3 node triangle
+
+    elemTags, elemNodeTags = gmsh.model.mesh.getElementsByType(
+        linear_tri_elem_type,
+        tag=-1,
+    )
+
+    xyz_nodeCoords = nodeCoords.reshape((-1, 3))
+    elemNodeTags = elemNodeTags.reshape((-1, 3))
 
     gmsh.finalize()
 
+    return nodeTags, xyz_nodeCoords, elemTags, elemNodeTags
+
+
+@njit
+def get_mesh_props(nodeTags, xyz_nodeCoords, elemTags, elemNodeTags):
+    # ! Reshape in the mesh generation function
+    # Reshape nodeCoords to [x,y,z]
+    # xyz_nodeCoords = nodeCoords.reshape((-1, 3))
+    # elemNodeTags = elemNodeTags.reshape((-1, 3))  # reshape the elemNodeTags
+
+    # Initialize numpy array to hold [node tag, x, y, x]
+    # Each row correponds to a node from the mesh
+    # Note: you must subtract 1 when using this mapping
+    # because gmsh ordering is human indexed.
+    map_node_tag_to_xyz = np.zeros((len(nodeTags), 4))
+    for i in range(len(nodeTags)):
+        tag_i = nodeTags[i]
+        x_i = xyz_nodeCoords[i, 0]  # x coord
+        y_i = xyz_nodeCoords[i, 1]  # y coord
+        z_i = xyz_nodeCoords[i, 2]  # z coord
+
+        # Update the array that maps node tag to xyz coord
+        map_node_tag_to_xyz[i, 0] = tag_i
+        map_node_tag_to_xyz[i, 1] = x_i
+        map_node_tag_to_xyz[i, 2] = y_i
+        map_node_tag_to_xyz[i, 3] = z_i
+
+    # Initialize numpy array that maps the gmsh node tag to start at 1.
+    # Note that the new ordering starts at value of 1
+    # (human indexed for the tags)
+    connectivity_array = np.zeros((len(elemTags), 5))
+    for i in range(len(elemTags)):
+        tag_i = int(elemTags[i])  # extract the gmsh node tag
+        n1 = int(elemNodeTags[i, 0])  # gmsh node 1 tag
+        n2 = int(elemNodeTags[i, 1])  # gmsh node 2 tag
+        n3 = int(elemNodeTags[i, 2])  # gmsh node 3 tag
+
+        n1_x = map_node_tag_to_xyz[n1 - 1][1]
+        n1_y = map_node_tag_to_xyz[n1 - 1][2]
+
+        n2_x = map_node_tag_to_xyz[n2 - 1][1]
+        n2_y = map_node_tag_to_xyz[n2 - 1][2]
+
+        n3_x = map_node_tag_to_xyz[n3 - 1][1]
+        n3_y = map_node_tag_to_xyz[n3 - 1][2]
+
+        # compute the area of each element
+        area_e = 0.5 * (
+            n1_x * (n2_y - n3_y) + n2_x * (n3_y - n1_y) + n3_x * (n1_y - n2_y)
+        )
+
+        # Ensure all elements are correctly oriented
+        if area_e < 0:
+            connectivity_array[i, 0] = i
+            connectivity_array[i, 1] = tag_i
+            connectivity_array[i, 4] = n1
+            connectivity_array[i, 3] = n2
+            connectivity_array[i, 2] = n3
+        else:
+            connectivity_array[i, 0] = i
+            connectivity_array[i, 1] = tag_i
+            connectivity_array[i, 2] = n1
+            connectivity_array[i, 3] = n2
+            connectivity_array[i, 4] = n3
+
+    return connectivity_array
+
 
 if __name__ == "__main__":
-    create_mesh()
+    # Generate the mesh using GMSH
+    t0_mesh = time.perf_counter()
+    nodeTags, xyz_nodeCoords, elemTags, elemNodeTags = create_mesh()
+    tf_mesh = time.perf_counter()
+
+    # Create the connectivity array
+    t0_mesh_prop = time.perf_counter()
+    connectivity_array = get_mesh_props(
+        nodeTags=nodeTags,
+        xyz_nodeCoords=xyz_nodeCoords,
+        elemTags=elemTags,
+        elemNodeTags=elemNodeTags,
+    )
+    tf_mesh_prop = time.perf_counter()
+
+    # Print out timed sections
+    time_gmsh = tf_mesh - t0_mesh
+    time_mesh_prop = tf_mesh_prop - t0_mesh_prop
+
+    # headers = ["Section", "Time (s)"]
+    # table = [
+    #     ["GMSH", time_gmsh],
+    #     ["Mesh Prop.", time_mesh_prop],
+    # ]
+    # print(tabulate(table, headers, tablefmt="fancy_outline"))
